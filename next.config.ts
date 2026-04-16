e71aa841c2e85e7f96598bb780a4eb10304e8f12
gabriel04rocha <gabriel04rocha@gmail.com>
2026-04-16 15:04:08 -0300

    chore: changing the next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
     dangerouslyAllowSVG: true,
     formats: ["image/avif", "image/webp"],
   },
+  webpack: (config) => {
+    config.optimization.splitChunks.automaticNameDelimiter = "-";
+    return config;
+  },
+  turbopack: {},
 };
 
 export default nextConfig;
